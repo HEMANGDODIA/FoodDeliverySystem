5616e554a30e5d703241150d18699c6df82fdf50
Hemang Dodiya <51086213+HEMANGDODIA@users.noreply.github.com>
2021-03-04 09:19:44 +0530

    auth system

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const mongoose = require('mongoose')
 const session = require('express-session')
 const flash = require('express-flash')
 const MongoDbStore = require('connect-mongo').default;
-
+const passport = require('passport')
 
 
 //Database connection
@@ -24,6 +24,9 @@ connection.once('open', () => {
 });
 
 
+
+
+//Session store
 const mongoStore = MongoDbStore.create({
     mongoUrl: url,
     collectionName: "sessions",
@@ -37,17 +40,26 @@ app.use(session({
     cookie: { maxAge: 1000 * 60 * 60 * 24 } //24 hours
 }))
 
+//Passport config
+const passportInit = require('./app/config/passport')
+passportInit(passport)
+app.use(passport.initialize())
+app.use(passport.session())
+
+
 app.use(flash())
 
 
 //Assets
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
 
 //Global middleware
 app.use((req, res, next) => {
     res.locals.session = req.session
+    res.locals.user = req.user
     next()
 })
 
